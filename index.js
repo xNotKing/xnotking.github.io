58375972bb0272f4be16d5f54b4d35ef09971603
xNotKing <87067415+xNotKing@users.noreply.github.com>
2022-06-26 18:25:04 +0000

    Fixed some UI issues.

--- index.js
+++
@@ -1,7 +0,0 @@
-let ads = ["replchat"]
-let random =  Math.floor(Math. random() * (4 - 1 + 1)) + 1
-console.log(random)
-/*if (random == 1) {
-    $(".ad-box").html(`<video src='ad-replchat.mp4' controls>`)
-    console.log("success")
-}*/
