7c9d373563a91fba4dd36217b24760f7919a7162
xNotKing <87067415+xNotKing@users.noreply.github.com>
2022-08-24 12:49:35 +0000

    Fixed window state issue.

--- renderer.js
+++ renderer.js
@@ -2,6 +2,10 @@ $('a').click(function (event) {
 // Cancel event
 event.preventDefault();
 // Load page content
-window.history.pushState("", "", $(this).attr("href"));
-$('body').load($(this).attr('href'));
+if(!$(this).attr("href").startsWith("https://")) {
+	window.history.pushState("", "", $(this).attr("href"));
+	$('body').load($(this).attr('href'));
+} else {
+	location.href = ($(this).attr('href'));
+}
 }); 
