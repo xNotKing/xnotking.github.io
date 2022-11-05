ddafb672c572ba7936e0918b145e3db5c1dd78fa
xNotKing <87067415+xNotKing@users.noreply.github.com>
2022-11-05 10:38:05 +0100

    🦾SEO + 🪫Render ASYNC

--- renderer.js
+++ renderer.js
@@ -1,10 +1,11 @@
-$('a').click(function (event) {
+$('a').click(async function (event) {
 // Cancel event
 event.preventDefault();
 // Load page content
 if(!$(this).attr("href").startsWith("https://")) {
+	await $('body').load($(this).attr('href'));
 	window.history.pushState("", "", $(this).attr("href"));
-	$('body').load($(this).attr('href'));
+	console.log("Loaded")
 } else {
 	window.open($(this).attr('href'));
 }
