91ec5b1f3d3fa298df23b97df1c5d118008791ee
xNotKing <87067415+xNotKing@users.noreply.github.com>
2022-11-13 09:51:22 +0100

    🦾Some CSS and JS fixes.

--- renderer.js
+++ renderer.js
@@ -1,12 +1,13 @@
+//import $ from "https://cdn.skypack.dev/jquery?min"
 $('a').click(async function (event) {
-// Cancel event
-event.preventDefault();
-// Load page content
-if(!$(this).attr("href").startsWith("https://")) {
-	await $('body').load($(this).attr('href'));
-	window.history.pushState("", "", $(this).attr("href"));
-	console.log("Loaded")
-} else {
-	window.open($(this).attr('href'));
+	// Cancel event
+	event.preventDefault();
+	// Load page content
+	if(!$(this).attr("href").startsWith("https://")) {
+		await $('body').load($(this).attr('href'));
+		window.history.pushState("", "", $(this).attr("href"));
+		console.log("Loaded")
+	} else {
+		window.open($(this).attr('href'));
 }
 }); 
